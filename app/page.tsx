2b973cc418c275cd7b8b784c77d318a6442b5406
jakmoss <jakritmoss121@gmail.com>
2025-02-20 21:28:42 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,37 +5,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-12 row-start-2 items-center sm:items-start">
         {/* Hero Section */}
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+        <h1>Goodbye ไว้เจอกันใหม่</h1>
 
         {/* Instructions */}
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-mono">
-          <li className="mb-2">
-            HI{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+       
 
         {/* Call to Action Links */}
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full flex items-center justify-center bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] h-10 sm:h-12 px-4 sm:px-5 transition-colors"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className="dark:invert mr-2" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 transition-colors"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+       
 
         {/* Video Section */}
         <section className="w-full max-w-4xl mx-auto grid gap-8 sm:grid-cols-2">
